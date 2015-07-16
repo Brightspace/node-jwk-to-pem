85e4bd0e36c23ecdb7c8ec5dc9e3d67063ebfea4
Owen Smith <owen@omsmith.ca>
2015-07-16 00:01:48 -0400

    add support for P-384 and P-521. Fixes #1. Fixes #2

--- spec/ec.js
+++ spec/ec.js
@@ -29,6 +29,49 @@ describe('ecdsa', function () {
 		});
 	});
 
+	describe('P-384', function () {
+		it('should convert a public JWK to a public PEM', function () {
+			var jwk = {
+				crv: 'P-384',
+				kty: 'EC',
+				x: 'QIRvRhN2MpnTQ4teO4Y_RYFaK2Qlvc2lbhC0vALwrFOy33kUihkNUvHiTaUsp2W3',
+				y: 'vSA1sCKKzT4UOavStUL2WpwcCflEyDshzy3dc1IZtACUngU2xMDDMsi0gDL9jLiU'
+			};
+
+			var expected =
+				'-----BEGIN PUBLIC KEY-----\n'
+				+ 'MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEQIRvRhN2MpnTQ4teO4Y/RYFaK2Qlvc2l\n'
+				+ 'bhC0vALwrFOy33kUihkNUvHiTaUsp2W3vSA1sCKKzT4UOavStUL2WpwcCflEyDsh\n'
+				+ 'zy3dc1IZtACUngU2xMDDMsi0gDL9jLiU\n'
+				+ '-----END PUBLIC KEY-----\n'
+			;
+
+			expect(jwkToPem(jwk)).to.equal(expected);
+		});
+	});
+
+	describe('P-521', function () {
+		it('should convert a public JWK to a public PEM', function () {
+			var jwk = {
+				crv: 'P-521',
+				kty: 'EC',
+				x: 'AFqLf9vO672gS-Lv_BabqzKoedNLQgZkCemRZuzYu4KJjHgPBZ5fs3S05MoRXl4e7lR026XDDNPXawySVDXta9KF',
+				y: 'APbUNzQ7IP_Mi0XwLN_RWZcIyHI43MJIAEn7O-KS0r8lvxjnVXeoopWAdqfTX_fCHXpYN1Ux1soOWujXb1uCEb7G'
+			};
+
+			var expected =
+				'-----BEGIN PUBLIC KEY-----\n'
+				+ 'MIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQAWot/287rvaBL4u/8FpurMqh500tC\n'
+				+ 'BmQJ6ZFm7Ni7gomMeA8Fnl+zdLTkyhFeXh7uVHTbpcMM09drDJJUNe1r0oUA9tQ3\n'
+				+ 'NDsg/8yLRfAs39FZlwjIcjjcwkgASfs74pLSvyW/GOdVd6iilYB2p9Nf98Idelg3\n'
+				+ 'VTHWyg5a6NdvW4IRvsY=\n'
+				+ '-----END PUBLIC KEY-----\n'
+			;
+
+			expect(jwkToPem(jwk)).to.equal(expected);
+		});
+	});
+
 	describe('should throw for', function () {
 		it('missing crv', function () {
 			var jwk = { kty: 'EC', x: 'foo', y: 'bar' };
