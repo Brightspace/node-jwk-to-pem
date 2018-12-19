f365562aaa15785ba3e3406abcba1ebf3be4feb6
Shinigami <chrissi92@hotmail.de>
2018-12-19 03:25:57 +0100

    Add JSDoc with TypeDef to jwkToBuffer (#29)
    * add jsdoc with typedef to jwkToBuffer
    
    * redefine EC typedef

--- src/jwk-to-pem.js
+++ src/jwk-to-pem.js
@@ -3,6 +3,12 @@
 var ec = require('./ec'),
 	rsa = require('./rsa');
 
+/**
+ *
+ * @param {{kty:'EC', crv:string, d:string, x?:string, y?:string} | {kty:'EC', crv:string, x:string, y:string} | {kty:'RSA', e:string, n:string, d?:string, p?:string, q?:string, dp?:string, dq?:string, qi?:string}} jwk
+ * @param {{private:boolean}=} opts
+ * @returns {string}
+ */
 function jwkToBuffer(jwk, opts) {
 	if ('object' !== typeof jwk || null === jwk) {
 		throw new TypeError('Expected "jwk" to be an Object');
